afe9181a76e74478ffc26d4abad9c36fdf318de6
TenYears <31788554+weizhengfu@users.noreply.github.com>
2026-01-17 16:46:59 +0800

    refactor: rewrite Main class with new architecture

--- src/main/java/cn/baruto/Main.java
+++ src/main/java/cn/baruto/Main.java
@@ -1,131 +1,53 @@
 package cn.baruto;
 
-import cn.hutool.core.collection.ListUtil;
-import cn.hutool.core.date.DateUtil;
+import cn.baruto.config.ConfigLoader;
+import cn.baruto.config.RenameConfig;
+import cn.baruto.core.ContentReplacer;
+import cn.baruto.core.PathCalculator;
+import cn.baruto.processor.FileProcessor;
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.ZipUtil;
-import cn.hutool.setting.dialect.Props;
-import cn.hutool.setting.dialect.PropsUtil;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        Props props = PropsUtil.get("setting.properties");
-        String zipFilePath = props.getProperty("zip.name");
+        // 1. 加载并验证配置
+        RenameConfig config = ConfigLoader.load("setting.properties");
 
-        // 获取到解压之后的代码路径
-        String codeDir = unzip(zipFilePath);
+        // 2. 初始化处理器
+        ContentReplacer contentReplacer = new ContentReplacer(config);
+        PathCalculator pathCalculator = new PathCalculator(config);
+        FileProcessor fileProcessor = new FileProcessor(contentReplacer, pathCalculator, config);
 
+        // 3. 解压源文件
+        String codeDir = unzip(config.getZipFilePath());
         File codeDirFile = new File(codeDir);
-        String targetDir = props.getProperty("target.path") + File.separator
-                + DateUtil.format(new Date(), "yyyyMMddHHmmss");
 
-        String targetPackageName = props.getProperty("package.name");
+        // 4. 遍历处理文件
         List<File> files = FileUtil.loopFiles(codeDirFile);
         for (File file : files) {
-            if (file.getName().equals("pom.xml")) {
-                // 修改pom.xml
-                renamePomXml(file, codeDirFile, targetDir, targetPackageName);
-            } else if (file.getName().endsWith(".java")) {
-                // 修改java文件
-                renameJava(file, codeDirFile, targetDir, targetPackageName);
-            } else if (file.getName().endsWith("Mapper.xml")) {
-                // 修改mapperXml文件
-                renameMapperXml(file, codeDirFile, targetDir, targetPackageName);
-            } else {
-                // 复制其他文件
-                renameOther(file, codeDirFile, targetDir, targetPackageName);
-            }
+            fileProcessor.processFile(file, codeDirFile);
         }
 
-        System.out.println("改名完成，新的文件路径：" + targetDir);
+        System.out.println("\n改名完成，输出目录：" + config.getOutputDirectory());
 
-        // 删除解压的文件
+        // 5. 清理临时文件
         FileUtil.del(codeDirFile);
     }
 
-    private static void renamePomXml(File pomFile, File codeDirFile, String targetPath, String targetPackageName) {
-        // 修改pom.xml
-        String fileContent = FileUtil.readUtf8String(pomFile);
-        String newContent = fileContent.replaceAll("org.dromara", targetPackageName);
-        if (newContent.contains("sms4j")) {
-            newContent = newContent.replaceAll(targetPackageName + ".sms4j", "org.dromara.sms4j");
-        }
-
-        String targetFileName = getTargetFileName(pomFile, codeDirFile, targetPath, targetPackageName);
-        FileUtil.writeString(newContent, targetFileName, "UTF-8");
-        System.out.println("写入文件：" + targetFileName);
-    }
-
-    private static void renameMapperXml(File mapperFile, File codeDirFile, String targetPath,
-            String targetPackageName) {
-        // 修改pom.xml
-        String fileContent = FileUtil.readUtf8String(mapperFile);
-        String newContent = fileContent.replaceAll("org.dromara", targetPackageName);
-
-        String targetFileName = getTargetFileName(mapperFile, codeDirFile, targetPath, targetPackageName);
-        FileUtil.writeString(newContent, targetFileName, "UTF-8");
-        System.out.println("写入文件：" + targetFileName);
-    }
-
-    private static void renameJava(File javaFile, File codeDirFile, String targetPath, String targetPackageName) {
-        // 修改pom.xml
-        String fileContent = FileUtil.readUtf8String(javaFile);
-        String newContent = fileContent.replaceAll("org.dromara", targetPackageName);
-        if (newContent.contains("sms4j")) {
-            newContent = newContent.replaceAll(targetPackageName + ".sms4j", "org.dromara.sms4j");
-        }
-        String targetFileName = getTargetFileName(javaFile, codeDirFile, targetPath, targetPackageName);
-        FileUtil.writeString(newContent, targetFileName, "UTF-8");
-        System.out.println("写入文件：" + targetFileName);
-    }
-
-    private static void renameOther(File file, File codeDirFile, String targetPath, String targetPackageName) {
-        // 修改pom.xml
-        String fileContent = FileUtil.readUtf8String(file);
-        String newContent = fileContent.replaceAll("org.dromara", targetPackageName);
-
-        String targetFileName = getTargetFileName(file, codeDirFile, targetPath, targetPackageName);
-        FileUtil.writeString(newContent, targetFileName, "UTF-8");
-        System.out.println("写入文件：" + targetFileName);
-    }
-
-    private static String getTargetFileName(File oldFile, File codeDirFile, String targetPath,
-            String targetPackageName) {
-        String codeDirFileAbsolutePath = getFilePath(codeDirFile);
-        String oldFileAbsolutePath = getFilePath(oldFile);
-
-        String keywords = Arrays.asList("src", "main", "java", "org", "dromara").stream()
-                .collect(Collectors.joining("/"));
-
-        List<String> targetKeywordList = ListUtil.toList("src", "main", "java");
-        String[] split = targetPackageName.split("\\.");
-        targetKeywordList.addAll(Arrays.asList(split));
-
-        String keywordsTarget = String.join("/", targetKeywordList);
-        if (oldFileAbsolutePath.contains(keywords)) {
-            oldFileAbsolutePath = oldFileAbsolutePath.replaceAll(keywords, keywordsTarget);
-        }
-
-        String relativePath = oldFileAbsolutePath.replace(codeDirFileAbsolutePath, "");
-        String result = targetPath + File.separator + relativePath;
-        return result;
-    }
-
+    /**
+     * 解压 ZIP 文件
+     */
     private static String unzip(String zipFilePath) {
-        // 创建一个本地临时文件夹
+        // 创建临时文件夹
         String tempFilePath = System.getProperty("user.dir") + "/temp/ruoyi_" + System.currentTimeMillis();
 
         File zipFile = new File(zipFilePath);
 
-        // 获取文件名，不带后缀的
-
+        // 获取文件名（不带后缀）
         String suffix = "." + FileUtil.extName(zipFilePath);
         String zipFileName = FileUtil.getName(zipFilePath).replaceAll(suffix, "");
 
@@ -133,13 +55,7 @@ public class Main {
         targetDir.mkdirs();
         ZipUtil.unzip(zipFile, targetDir);
 
-        // 解压出来的代码路径
+        // 返回解压后的代码路径
         return tempFilePath + "/" + zipFileName;
     }
-
-    private static String getFilePath(File file) {
-        String absolutePath = file.getAbsolutePath();
-        return absolutePath.replaceAll("\\\\", "/");
-    }
-
 }
