ec2179ae881a34f323211aef5bec879397cb2d47
TenYears <31788554+weizhengfu@users.noreply.github.com>
2026-01-17 16:42:43 +0800

    feat: add ContentReplacer engine

---
+++ src/main/java/cn/baruto/core/ContentReplacer.java
@@ -0,0 +1,65 @@
+package cn.baruto.core;
+
+import cn.baruto.config.RenameConfig;
+import cn.hutool.core.util.StrUtil;
+
+public class ContentReplacer {
+    private final RenameConfig config;
+
+    public ContentReplacer(RenameConfig config) {
+        this.config = config;
+    }
+
+    public String replaceContent(String content, String moduleName) {
+        String result = content;
+        result = replacePackageName(result, moduleName);
+        result = replaceModuleName(result);
+        return result;
+    }
+
+    private String replacePackageName(String content, String moduleName) {
+        String result = content;
+        if (PathUtils.isRetainModule(moduleName, config.getRetainModules())) {
+            result = replaceModulePrefixOnly(result);
+        } else {
+            result = result.replaceAll("org.dromara", config.getTargetPackageName());
+            if (config.getRetainModules() != null) {
+                for (String retain : config.getRetainModules()) {
+                    String wrongPattern = config.getTargetPackageName() + "." + retain;
+                    String correctPattern = "org.dromara." + retain;
+                    result = result.replaceAll(wrongPattern, correctPattern);
+                }
+            }
+        }
+        return result;
+    }
+
+    private String replaceModulePrefixOnly(String content) {
+        String result = content;
+        if (StrUtil.isNotBlank(config.getOldPrefix()) && StrUtil.isNotBlank(config.getNewPrefix())) {
+            result = result.replaceAll("<artifactId>" + config.getOldPrefix() + "-", "<artifactId>" + config.getNewPrefix() + "-");
+            result = result.replaceAll("</artifactId>", "</artifactId>");
+            result = result.replaceAll("<module>" + config.getOldPrefix() + "-", "<module>" + config.getNewPrefix() + "-");
+            result = result.replaceAll("</module>", "</module>");
+        }
+        if (config.getModuleMap() != null) {
+            for (java.util.Map.Entry<String, String> entry : config.getModuleMap().entrySet()) {
+                result = result.replaceAll(entry.getKey(), entry.getValue());
+            }
+        }
+        return result;
+    }
+
+    private String replaceModuleName(String content) {
+        String result = content;
+        if (config.getModuleMap() != null) {
+            for (java.util.Map.Entry<String, String> entry : config.getModuleMap().entrySet()) {
+                result = result.replaceAll(entry.getKey(), entry.getValue());
+            }
+        }
+        if (StrUtil.isNotBlank(config.getOldPrefix()) && StrUtil.isNotBlank(config.getNewPrefix())) {
+            result = result.replaceAll(config.getOldPrefix(), config.getNewPrefix());
+        }
+        return result;
+    }
+}
